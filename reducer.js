2dfad89ec396f65b7ffb4add6ed0592954a012b9
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-09-02 18:52:43 -0600

    transcribes guanine to cytosine

--- reducer.js
+++ reducer.js
@@ -1,3 +1,8 @@
 module.exports.transcribeDnaToRna = function (state = {strand: "G"}, action) {
+    if (action.dna === "G") {
+        return {
+            strand: "C"
+        }
+    }
     return state;
 };
