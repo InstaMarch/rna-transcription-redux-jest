d3812ee424621dc0efc24b7a90bb7cae506f2ef2
Zach Overhulser <zoverhulser@gmail.com>
2017-09-03 20:24:30 -0400

    Preventing transcription of guanine to cytosine

--- reducer.js
+++ reducer.js
@@ -1,8 +1,3 @@
 module.exports.transcribeDnaToRna = function (state = {strand: "G"}, action) {
-    if (action.dna === "G") {
-        return {
-            strand: "C"
-        }
-    }
     return state;
 };
