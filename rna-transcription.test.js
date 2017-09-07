29e8cbf978c185b4ccf384d2f33323906bd1f844
Zach Overhulser <zoverhulser@gmail.com>
2017-09-07 00:27:48 -0400

    testing transcription from adenine to uracil

--- rna-transcription.test.js
+++ rna-transcription.test.js
@@ -5,3 +5,8 @@ test("transcribes cytosine to guanine", function () {
     store.dispatch(transcribeDnaToRna("C"));
     expect(store.getState().strand).toEqual("G");
 });
+
+test("transcribes adenine to uracil", function () {
+    store.dispatch(transcribeDnaToRna("A"));
+    expect(store.getState().strand).toEqual("U");
+});
