578b0ab4d7f348f912d5a6fa65816fe09f88eed5
Zach Overhulser <zoverhulser@gmail.com>
2017-09-07 00:31:13 -0400

    Transcribes adenine to uracil

--- reducers.js
+++ reducers.js
@@ -1,3 +1,9 @@
 module.exports.transcribeDnaToRnaReducer = (state = {strand: "G"}, action) => {
+    if (action.dna === "A") {
+        return {
+	    strand: "U"
+	}
+    }
+
     return state;
 }; 
