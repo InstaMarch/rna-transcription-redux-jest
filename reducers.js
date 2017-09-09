90f1af6c77ff56ed3e05c3765f6ebd425e22fc0a
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-09-09 17:18:22 -0600

    Transcribes whole strand of DNA to RNA

--- reducers.js
+++ reducers.js
@@ -1,28 +1,43 @@
-module.exports.transcribeDnaToRnaReducer = (state = {strand: "G"}, action) => {
-    if (action.dna === "A") {
-        return {
-	    strand: "U"
-	}
-    }
+const transcribeDnaToRnaReducer = (state = {strand: "G"}, action) => {
+    if (action !== undefined && action.type === "TRANSCRIBE_DNA_TO_RNA") {
+        if (typeof action.dna === "string" && action.dna.length !== 1) {
+            const reducedRightStrandAction = Object.assign({}, action, {
+                dna: action.dna.slice(1, action.dna.length)
+            });
+            const reducedLeftSingleStrandAction = Object.assign({}, action, {
+                dna: action.dna.slice(0, 1)
+            });
 
-    if (action.dna === "T") {
-        return {
-	    strand: "A"
-	}
-    }
+            return {
+                strand: transcribeDnaToRnaReducer(state, reducedLeftSingleStrandAction).strand
+                + transcribeDnaToRnaReducer(state, reducedRightStrandAction).strand
+            };
+        }
+        if (action.dna === "A") {
+            return {
+                strand: "U"
+            }
+        }
+if (action.dna === "T") {
+            return {
+                strand: "A"
+            }
+        }
 
-    if (action.dna === "C") {
+        if (action.dna === "C") {
+            
+            return {
+                strand: "G"
+            }
+        }
         
-	return {
-	    strand: "G"
-	}
+        if (action.dna === "G") {
+            return {
+                strand: "C"
+            }
+        }
     }
-    
-    if (action.dna === "G") {
-        return {
-	    strand: "C"
-	}
-    }
-
     return state;
-}; 
+};
+
+module.exports.transcribeDnaToRnaReducer = transcribeDnaToRnaReducer;
