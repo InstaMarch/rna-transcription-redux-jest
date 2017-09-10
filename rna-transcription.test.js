9981f66bb6f9f336c07658ebea72766bdb1d1852
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-09-09 18:39:32 -0600

    Tests the transcription of adenine to uracil

--- rna-transcription.test.js
+++ rna-transcription.test.js
@@ -1,8 +1,12 @@
 const {store} = require("./store");
 const {transcribeDnaToRna} = require("./action");
 
-test("transcribes cytosine to guanine", function () {
+test("Transcribes cytosine to guanine", function () {
     store.dispatch(transcribeDnaToRna("C"));
     expect(store.getState().strand).toEqual("G");
 });
 
+test("Transcribes adenine to uracil", function () {
+    store.dispatch(transcribeDnaToRna("A"));
+    expect(store.getState().strand).toEqual("U");
+});
