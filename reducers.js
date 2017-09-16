bedf6a1125fca57aa63d05b38a590f643ff89cf6
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-09-16 17:44:38 -0600

    Transcribes a whole strand DNA strand to its RNA compliment.

--- reducers.js
+++ reducers.js
@@ -1,18 +1,28 @@
+const {transcribeDnaToRna} = require("./action");
+
 const transcribeDnaToRnaReducer = function (state = {
   strand: "G"
 }, action) {
+    if(action && action.dna && action.dna.length > 1) {
+        const singleItemStrand = transcribeDnaToRnaReducer(state, transcribeDnaToRna(action.dna.slice(0, 1))).strand;
+        const manyItemStrand =  transcribeDnaToRnaReducer(state, transcribeDnaToRna(action.dna.slice(1, action.dna.length))).strand;
+        return {
+            strand:  singleItemStrand + manyItemStrand
+        }
+    }
     if (action.dna === "A") {
-        return {strand: "U"}
+        return {strand: "U"};
     }
 
     if (action.dna === "G") {
-        return {strand: "C"}
+        return {strand: "C"};
     }
     if (action.dna === "T") {
-        return {strand: "A"}
+        return {strand: "A"};
     }
-    
-
+    if (action.dna === "C") {
+        return {strand: "G"};
+    }
     return state;
 };
 module.exports.transcribeDnaToRnaReducer = transcribeDnaToRnaReducer;
