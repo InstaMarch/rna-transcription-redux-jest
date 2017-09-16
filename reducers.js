2e6ba49ab5065162feb4b8add03ff713e545b980
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-09-16 17:49:44 -0600

    Removes string to string transcription.

--- reducers.js
+++ reducers.js
@@ -3,13 +3,6 @@ const {transcribeDnaToRna} = require("./action");
 const transcribeDnaToRnaReducer = function (state = {
   strand: "G"
 }, action) {
-    if(action && action.dna && action.dna.length > 1) {
-        const singleItemStrand = transcribeDnaToRnaReducer(state, transcribeDnaToRna(action.dna.slice(0, 1))).strand;
-        const manyItemStrand =  transcribeDnaToRnaReducer(state, transcribeDnaToRna(action.dna.slice(1, action.dna.length))).strand;
-        return {
-            strand:  singleItemStrand + manyItemStrand
-        }
-    }
     if (action.dna === "A") {
         return {strand: "U"};
     }
