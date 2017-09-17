c54af5c19634e6e3cb8b835e8d0ad4448799fc16
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-09-16 18:08:22 -0600

    Removes transcription from Adenine to Uracil.

--- reducers.js
+++ reducers.js
@@ -3,9 +3,6 @@ const {transcribeDnaToRna} = require("./action");
 const transcribeDnaToRnaReducer = function (state = {
   strand: "G"
 }, action) {
-    if (action.dna === "A") {
-        return {strand: "U"};
-    }
 
     return state;
 };
